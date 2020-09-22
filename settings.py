686e22c63843b84b77611596fc48bf1249e7846e
Dennis <d.m.m.v.d.vorst@student.tue.nl>
2020-09-22 16:21:10 +0200

    -

--- settings.py
+++ settings.py
@@ -17,7 +17,7 @@ path_freecad_server = '/home/tue/s111167/freecad/lib'
 
 # model settings
 export_filename_prefix = 'model_'
-n_objects = randint(low=10, high=20)
+n_objects = randint(low=10, high=15)
 limit_placement = [-50, 50]  # mm
 limit_scale = [10, 100]  # mm
 boundary_radius = 100  # mm
