d30e03fdb9af6867a7fc4d07c57c38b20f26032b
Dennis <d.m.m.v.d.vorst@student.tue.nl>
2020-09-22 16:56:34 +0200

    -

--- task.sh
+++ task.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/bash
 
 module activate anaconda
-conda activate '/home/tue/s111167/.conda/envs/conda-env'
-conda list
+source activate '/home/tue/s111167/.conda/envs/conda-env'
 python main.py
