6c4daa89748a2f1fdcad9cb3b308c31c76cb7c75
Dennis <d.m.m.v.d.vorst@student.tue.nl>
2020-09-24 11:00:59 +0200

    -

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import argparse
 import settings
 from util.generate_cst_project import generate_cst_project
 
+# on the server, the job-id is passed as an argument, use job-id 1 on desktop
 if settings.is_running_on_desktop:
     job_id = 1
 else:
@@ -10,5 +11,6 @@ else:
     parser.add_argument("--job_id", help="server job id-number", type=int)
     job_id = parser.parse_args().job_id
 
-for idx in range(0, 300):
+# generate cst_projects
+for idx in range(0, settings.n_projects):
     generate_cst_project(job_id)
--- settings.py
+++ settings.py
@@ -6,10 +6,9 @@ import util.constants as constants
 
 # project settings
 project_root_folder_desktop = 'C:/Users/Dennis/Documents/generated_projects'
-project_root_folder_server = '/home/tue/s111167/generated_projects'
+project_root_folder_server = '/home/tue/s111167/generated_projects2'
 project_folder_prefix = 'project_'
 project_folder_name_n_zero_padding = 5
-max_projects = 10000
 is_running_on_desktop = os.name == 'nt'  # nt: Windows , posix: Linux
 path_cst_exe_desktop = 'C:/Program Files (x86)/CST Studio Suite 2020/' \
                        'CST DESIGN ENVIRONMENT.exe'
@@ -17,14 +16,22 @@ path_cst_exe_server = '/cm/shared/apps/cst/CST_STUDIO_SUITE_2020' \
                       '/cst_design_environment'
 path_freecad_desktop = 'C:/Program Files/FreeCAD/FreeCAD_0.19.22474/bin'
 path_freecad_server = '/home/tue/s111167/freecad/lib'
+max_projects = 10000
+if is_running_on_desktop:
+    n_projects = 1
+else:
+    n_projects = 300
 
 # model settings
 export_filename_prefix = 'model_'
-n_objects = randint(low=8, high=len(constants.Materials)-1)
 limit_placement = [-50, 50]  # mm
 limit_scale = [10, 100]  # mm
 boundary_radius = 100  # mm
 boundary_height = [-50, 50]  # mm
+if is_running_on_desktop:
+    n_objects = 2
+else:
+    n_objects = randint(low=8, high=len(constants.Materials)-1)
 
 # object settings
 f_max = 15
