94c48b3d752c209967045f7cedd32f9dbea6d968
Dennis <d.m.m.v.d.vorst@student.tue.nl>
2020-09-28 15:50:26 +0200

    -

--- server.sh
+++ server.sh
@@ -28,7 +28,6 @@ do
           --nodes=1 \
           --ntasks=1 \
           --cpus-per-task=4 \
-          --gres=gpu:1 \
           --time=2-00:00:00 \
           --partition=elec-em.gpu.q \
           --output=output/t_$job_id \
