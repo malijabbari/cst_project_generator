596cf73f1aa04c59276db95ef3159e7540f7d277
Dennis <d.m.m.v.d.vorst@student.tue.nl>
2020-09-29 11:31:12 +0200

    -

--- server.sh
+++ server.sh
@@ -46,6 +46,8 @@ declare -a partitions=("tue.default.q"
                        "elec.gpu.q"
                        "elec-em.gpu.q")
 
+echo "check"
+
 # execute jobs
 for (( job_id=0; job_id<$1; job_id++ ))
 do
